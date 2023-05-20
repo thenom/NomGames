a1488076bab0d3e79a0555aac8f3ae34342e2784
Simon Thorley <simon@thenom.co.uk>
2023-05-20 09:35:15 +0100

    Initial

---
+++ nomgames/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "nomgamesf70b7c5e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ nomgames/android/app/src/main/kotlin/com/example/nomgames/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nomgames
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
